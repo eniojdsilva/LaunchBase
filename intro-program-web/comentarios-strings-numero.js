3ed4b77eca69403168508ac6c55a3f95783af672
Enio Silva <eniojdsilva@hotmail.com>
2020-06-15 15:15:45 -0300

    Criado variáveis constantes com nome e média dos alunos depois realizado o calculo

---
+++ intro-program-web/comentarios-strings-numero.js
@@ -0,0 +1,14 @@
+// Comentários com duas barras
+// Criar um programa que calcula a média
+// das notas entre os alunos e enviar mensagem do cálculo da média
+
+const aluno01 = 'Enio'
+const aluno02 = 'Silva'
+const aluno03 = 'Rodrigo'
+const notaAluno01 = 5.8
+const notaAluno02 = 20
+const notaAluno03 = 2
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
+
+console.log(media)
